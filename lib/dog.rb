0fad7a3f8ba3f22d72a7c835f7c0b946d2742d56
divbeech <dyadav1006@gmail.com>
2019-09-17 10:19:16 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+class Dog
+def initialize(name)
+    @name = name
+end
+end
+dog = Dog.new ("Fido")
+dog = Dog.new ("Snoopy")
+dog = Dog.new ("Lassie")
+p dog 
---
+++ lib/person.rb
@@ -0,0 +1,10 @@
+class Person
+    def personality(name)
+        @name = name
+end
+
+end
+
+person = Person.new ("adele_goldberg")
+person = Person.new ("alan_kay")
+p person
